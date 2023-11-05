ba12ff9b61e30ba3818d8d674d31dfddfda07f5d
Alexandr Uvaev <alex211275@yandex.ru>
2023-11-05 13:10:46 +0300

    Добавьте файлы проекта.

---
+++ Less_8_Itogi_1/DeleteObjects.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less_8_Itogi_1
+{
+    internal class DeleteObjects
+    {
+        // класс для удаления папок и / или файлов
+        // поля класса
+        private string strPathInMet;
+
+        // меетод класса
+        public void delObj(string strPath)
+        {
+            strPathInMet = strPath;
+
+            // проверяем есть ли по такому пути исходная папка
+            if (Directory.Exists(strPathInMet))
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(strPathInMet);
+                
+                // работа по удалению папок
+                DirectoryInfo[] dirs = dirInfo.GetDirectories();
+
+                if (dirs.Length > 0)
+                {
+                    DateTime timeLastUse;
+                    DateTime timeNow = DateTime.Now;
+                    int i = 0;
+                    foreach (DirectoryInfo dir in dirs)
+                    {
+                        timeLastUse = dir.LastAccessTime;
+                        if ((timeNow - timeLastUse) > TimeSpan.FromMinutes(30))
+                        {
+                            dir.Delete(true);
+                            i++;
+                        }
+                    }
+                    Console.WriteLine("Удалено {0} пап(ки/ок)", i);
+                }
+                else 
+                {
+                    Console.WriteLine("В выбраной директории ни одной папки не обнаружено!");
+                }
+
+                // работа по удалению файлов
+                FileInfo[] files = dirInfo.GetFiles();
+                if (files.Length > 0)
+                {
+                    DateTime timeLastUse;
+                    DateTime timeNow = DateTime.Now;
+                    int i = 0;
+
+                    foreach (FileInfo file in files)
+                    {
+                        timeLastUse = file.LastAccessTime;
+                        if ((timeNow - timeLastUse) > TimeSpan.FromMinutes(30))
+                        {
+                            file.Delete();
+                            i++;
+                        }
+                    }
+                    Console.WriteLine("Удалено {0} фай(л/ов)", i);
+                }
+                else
+                {
+                    Console.WriteLine("В выбраной директории ни одного файла не обнаружено!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("По исходному пути нужная директория не  найдена! Для завершения программы нажмите любую клавишу!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Less_8_Itogi_1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Less_8_Itogi_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // объявляем переменную с путем до нужного ресурса
+            string strPath = "D:\\Модуль_8";
+            string answerUser;
+
+            Console.WriteLine("Вы находитесь в программе по очистке места на диске!");
+            Console.WriteLine("Программа удаляет папки и файлы по следующему пути: {0}", strPath);
+            Console.WriteLine("По указанному пути будут удалены все файлы и папки, которые");
+            Console.WriteLine("не использлвались в течение последних 30 минут!");
+            Console.WriteLine();
+
+            Console.Write("Вы хотите продолжить? Ответте ДА / НЕТ: ");
+            answerUser = Console.ReadLine();
+
+            if (answerUser == "ДА")
+            {
+                // вызываем метод для очистки указанной папки
+                // в метод будем передавать путь до папки
+                DeleteObjects deleteObjects = new DeleteObjects();
+                deleteObjects.delObj(strPath);
+            }
+            else
+            {
+                Console.WriteLine("Нажмите любую клавишу для закрытия данной программы!");
+                Console.ReadKey();
+            }
+
+
+
+        }
+    }
+}
